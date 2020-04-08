a591ba96459939e253e5aa3a6a377e3b905068f0
Piotr Hejwowski <piotr.hejwowski@softwaremill.pl>
2020-04-08 14:26:20 +0200

    pdfMe - now using public docker image

--- pdfMe.sh
+++ pdfMe.sh
@@ -5,10 +5,11 @@ SITE_URL=$1
 PDF_NAME=$2
 
 if [ ! -z "$SITE_URL" ] || [ ! -z "$PDF_NAME" ]; then
-  docker run --rm -it --user $(id -u):$(id -g) -v $(pwd):/pandoc --name a2 pandoc-util \
+  echo "Downloading $SITE_URL as $PDF_NAME..."
+  docker run --rm -it --user $(id -u):$(id -g) -v $(pwd):/pandoc --name pandoc1 hejwo/pandoc-web-to-pdf-util \
   pandoc --highlight-style tango --pdf-engine=xelatex -V geometry:margin=1.5cm -s -r html $SITE_URL -o $PDF_NAME
 else
-  echo "Usage: pdfMe http://site.to.download pdf-name.pdf"
+  echo "Usage: pdfMe http://site.to.download.com pdf-name.pdf"
 fi
 
 
